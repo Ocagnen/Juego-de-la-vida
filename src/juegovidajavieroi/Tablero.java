65144814cd89e81c6f1a02f1bcf4a246c4c93a9b
Javier <ocagnen2@gmail.com>
2019-04-01 08:47:58 +0200

    Crea atributos,constructores, etc

--- src/juegovidajavieroi/Tablero.java
+++ src/juegovidajavieroi/Tablero.java
@@ -11,4 +11,29 @@ package juegovidajavieroi;
  */
 public class Tablero {
     
+    private boolean[][] tablero;
+
+    public Tablero(int n) {        
+        
+        this.tablero = new boolean[n][n];
+    }
+    
+    public boolean[][] getTablero() {
+        return tablero;
+    }
+
+    public void setTablero(boolean[][] tablero) {
+        this.tablero = tablero;
+    }
+    
+    public void mostrarTablero(){
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Tablero" + "tablero=" + tablero ;
+    }
+    
+    
 }
