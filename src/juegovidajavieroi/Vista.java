b9384c0d94a2da23dfc66c88ca54872e0fe2ee91
Javier <ocagnen2@gmail.com>
2019-04-01 09:18:43 +0200

    Empieza a crear métodos para mostrar preguntas al usuario

--- src/juegovidajavieroi/Vista.java
+++ src/juegovidajavieroi/Vista.java
@@ -5,10 +5,38 @@
  */
 package juegovidajavieroi;
 
+import java.util.Scanner;
+
 /**
  *
  * @author javier
  */
 public class Vista {
+
+    public static Scanner tec = new Scanner(System.in);
+
+    public static Comando mostrarMenu() {
+
+        int respuesta;
+
+        System.out.println("JUEGO DE LA VIDA");
+
+        do {
+
+            System.out.println("----------------------------");
+            System.out.println("¿Qué deseas hacer?");
+            System.out.println("1.Partida nueva");
+            System.out.println("2.Cargar partida");
+            respuesta = tec.nextInt();
+
+        } while (respuesta > 2 || respuesta < 1);
+
+    }
     
+    public static int elegirTablero(){
+        
+        System.out.println("¿Qué dimensiones desea darle al tablero? (N x N)");
+        
+    }
+
 }
