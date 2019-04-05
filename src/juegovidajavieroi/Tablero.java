e320cdbc276556122daae8ae100aa4e13c708855
javier <ocagnen2@gmail.com>
2019-04-05 17:46:08 +0200

    Crea método para comprobar células en posición estándar

--- src/juegovidajavieroi/Tablero.java
+++ src/juegovidajavieroi/Tablero.java
@@ -131,8 +131,28 @@ public class Tablero {
 
     }
 
-    private int celEstandar() {
+    private int celEstandar(int fila, int columna) {
         int celulasVecinas = 0;
+        
+        if(this.tablero[fila-1][columna]){
+            celulasVecinas++;
+        } else if (this.tablero[fila-1][columna-1]){
+            celulasVecinas++;
+        } else if (this.tablero[fila-1][columna+1]){
+            celulasVecinas++;
+        }else if (this.tablero[fila][columna-1]){
+            celulasVecinas++;
+        }else if (this.tablero[fila][columna+1]){
+            celulasVecinas++;
+        }else if (this.tablero[fila+1][columna]){
+            celulasVecinas++;
+        } else if (this.tablero[fila+1][columna-1]){
+            celulasVecinas++;
+        } else if (this.tablero[fila+1][columna+1]){
+            celulasVecinas++;
+        }
+        
+        return celulasVecinas;
 
     }
 
